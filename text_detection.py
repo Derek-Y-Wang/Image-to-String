916a8740e926c565c0d1efeba3c58181ecbf5548
Derek Wang <wang.y.derek@gmail.com>
2020-09-09 01:22:11 -0400

    started making class

--- text_detection.py
+++ text_detection.py
@@ -28,6 +28,30 @@ for b in boxes.splitlines():
 cv2.imshow('Result', img)
 # cv2.imshow("l", img[])
 cv2.waitKey(0)
-# class SeperateCharacters:
-#
-#     def __init__(self, ):
+
+class SeperateCharacters:
+
+    def __init__(self, image):
+        try:
+            pytesseract.pytesseract.tesseract_cmd = "./pytesseract\\Tesseract-OCR\\tesseract.exe"
+            self.img = cv2.imread(image, cv2.IMREAD_GRAYSCALE)
+        except:
+            print("Missing pytesseract")
+
+
+    def get_letters(self):
+        hImg, wImg = self.img.shape
+        boxes = pytesseract.image_to_boxes(self.img)
+        letter_count = 0
+
+        for b in boxes.splitlines():
+            b = b.split(' ')
+            b[0] = letter_count
+            letter_count += 1
+            print(b)
+            # print(type(b))
+            x, y, w, h = int(b[1]), int(b[2]), int(b[3]), int(b[4])
+            cv2.rectangle(img, (x, hImg - y), (w, hImg - h), (0, 0, 255), 2)
+            # ROI = img[w:hImg - h, x:hImg - y]
+            # cv2.imwrite('./temp/ROI.png', ROI)
+
