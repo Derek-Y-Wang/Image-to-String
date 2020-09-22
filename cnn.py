75583a1488908b108a06171fc8be389c2facf9f8
Derek Wang <wang.y.derek@gmail.com>
2020-09-22 01:10:40 -0400

    added some scripts i used for testing code

--- cnn.py
+++ cnn.py
@@ -72,16 +72,3 @@ class LetterReader:
         return self.cnn.predict(test_image)
 
 
-# L = LetterReader()
-# L.load('model.h5')
-# files = os.listdir("dataset/letters/test_set/")
-# index_map = dict()
-# for n in range(len(os.listdir("dataset/letters/test_set/"))):
-#     index_map[n] = files[n]
-#
-# encoding = L.prediction("dataset/letters/single_prediction/ROI_3.PNG")
-#
-# print(encoding)
-# for i in range(len(encoding[0])):
-#     if encoding[0][i] >= 0.9:
-#         print(index_map[i])
